01cda6d6e6c3357514eebc2fa27f88f8b7a6e469
vskeerthi333 <vskeerthiswarna@gmail.com>
2020-06-08 10:34:55 +0530

    implementing Signature generation and verification

--- blockchain/utils/SignatureUtils.java
+++ blockchain/utils/SignatureUtils.java
@@ -1,5 +1,6 @@
 package blockchain.utils;
 
+import java.util.*;
 import java.security.*;
 
 public class SignatureUtils {
@@ -16,11 +17,28 @@ public class SignatureUtils {
     }
 
     public static String generateSignature(String data, PrivateKey privateKey) {
+        String signature = null;
 
+        try {
+            Signature sign = Signature.getInstance("SHA256withRSA");
+            sign.initSign(privateKey);
+            sign.update(data.getBytes("UTF-8"));
+            signature = Base64.getEncoder().encodeToString(sign.sign());
+        } catch (Exception ignored) { }
+
+        return signature;
     }
 
     public static boolean verifySignature(String data, String signature, PublicKey publicKey) {
+        boolean verification = false;
 
+        try {
+            Signature sign = Signature.getInstance("SHA256withRSA");
+            sign.initVerify(publicKey);
+            sign.update(data.getBytes("UTF-8"));
+            verification = sign.verify(Base64.getDecoder().decode(signature));
+        } catch (Exception ignored) { }
+
+        return verification;
     }
-
 }
