4047a9a43f0c414f19d21baace484dfdee055afb
Karthik Chennupati <edu.karthikch@gmail.com>
2020-06-19 19:12:31 +0530

    Cleaning Miner.java

--- blockchain/user/Miner.java
+++ blockchain/user/Miner.java
@@ -6,35 +6,35 @@ import blockchain.utils.*;
 
 public class Miner extends User implements Runnable {
 
-    private Block currentMiningBlock;
+    private Block currentMiningBlock = null;
     private static final int BLIND_REPETITIONS = 100;
     private static final int SLEEP_WHEN_NO_WORK_MS = 1000;
+    private Random mineRandom;
+    private Random selectRandom;
 
     private Miner(long id, Blockchain blockchain) {
         super(id, blockchain);
-        currentMiningBlock = null;
-        random = null;
+        selectRandom = new Random();
     }
 
-    static Miner with(long id, Blockchain blockchain) {
+    public static Miner with(long id, Blockchain blockchain) {
         return new Miner(id, blockchain);
     }
 
     @Override
     public void run() {
         while (true) {
-            random = new Random();
-            int randNum = random.nextInt();
+            int randNum = selectRandom.nextInt();
             if (randNum % id == 0) {
                 doTransaction();
             }
             else {
-                doMinning();
+                doMining();
             }
         } 
     }
 
-    private void doMinning() {
+    private void doMining() {
         boolean successful = false;
         
         while (!successful) {
@@ -49,7 +49,6 @@ public class Miner extends User implements Runnable {
             }
             successful = blindMining();
             if (successful) {
-                currentMiningBlock.setMinerId(id);
                 blockchain.submitBlock(currentMiningBlock, this);
             }
         }
@@ -58,7 +57,7 @@ public class Miner extends User implements Runnable {
     private boolean blindMining() {
         String requiredPrefix = blockchain.getRequiredPrefixForHash();
         for (int i = 0; i < BLIND_REPETITIONS; i++) {
-            currentMiningBlock.setMagicNum(random.nextInt());
+            currentMiningBlock.setMagicNum(mineRandom.nextInt());
             String computedHash = StringUtils.applySha256(currentMiningBlock.toString());
             if (computedHash.startsWith(requiredPrefix)) {
                 currentMiningBlock.setHash(computedHash);
@@ -72,7 +71,11 @@ public class Miner extends User implements Runnable {
         Block block = blockchain.getUnprocessedBlock();
         if (currentMiningBlock == null || !currentMiningBlock.equals(block)) {
             currentMiningBlock = block;
-            random = new Random();
+            mineRandom = new Random();
+
+            if (currentMiningBlock != null) {
+                currentMiningBlock.setMinerId(id);
+            }
         }
     }
 }
